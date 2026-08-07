7cf4e563302de18b12b13b06b84f11447e3b6baf
nbue <nbue@users.noreply.github.com>
2026-08-07 11:06:09 +0200

    V2.5 And global reroute

--- v2/main.py
+++ v2/main.py
@@ -1,43 +1,41 @@
+# v2
+
 # Uncomment the path for your settings, or use input.
-# path = "./prefrences/settings.json"
-# path = "./prefrences/settings.yaml"
-# path = "./prefrencessettings.toml"
-path = None
-if path == None:
-    path = input("enter path")
+path = "./../usage/main.json"
+# path = "./../usage/main.toml"
+# path = "./../usage/main.yaml"
+# path = None
+if path is None:
+    print("file.type supported by default: main.yaml, main.toml, main.json")
+    path = input("enter path (./../usage/main.type): ")
+if path.endswith((".json", ".yaml", ".toml")):
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            if path.endswith(".json"):
+                import json
 
+                cfg = json.load(file)
+            elif path.endswith(".yaml"):
+                import yaml
 
-with open(path, "r", encoding="utf-8") as f:
-    passed = False
-    if path.endswith("json"):
-        import json
+                cfg = yaml.safe_load(file)
 
-        cfg = json.load(f)
-        passed = True
-    elif path.endswith("yaml"):
-        import yaml
+            else:
+                import toml
 
-        cfg = yaml.load(f)
-        passed = True
+                cfg = toml.load(file)
+    except FileNotFoundError:
+        raise ValueError(f"{path} doesn't exist")
 
-    elif path.endswith("toml"):
-        import toml
-
-        cfg = toml.load(f)
-        passed = True
-    if passed is False:
-        print("Warning: no file given, supported formats: json, yaml, toml")
-    else:
-        sunrise = cfg.get("sunrise", None)
-
-        sunset = cfg.get("sunset", None)
-
-        degree = cfg.get("degree", None)
-        if sunrise == None:
-            print("sunrise not given, using average {}")
-        if sunset == None:
-            print("sunrise not given, using average {}")
-        if degree == None:
-            raise TypeError("No Input For degrees, can't calculate.")
-
-print(sunrise + degree / 180 * (sunset - sunrise))
+    if cfg is None:
+        raise ValueError("Config file is empty")
+    sunrise = cfg.get("sunrise")
+    sunset = cfg.get("sunset")
+    degree = cfg.get("degree")
+    if sunrise is None or sunset is None or degree is None:
+        raise ValueError(
+            "one or more inputs not found. verify: https://github.com/thenbue/time-calculator"
+        )
+    print(sunrise + degree / 180 * (sunset - sunrise))
+else:
+    raise ValueError(f"{path} doesn't work with this file.")
